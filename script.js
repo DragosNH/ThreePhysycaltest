2249c1888e81917c244e3534e15b4431966e27d2
Dragoş <edragos33@gmail.com>
2025-06-02 17:49:03 +0200

    comments added

--- script.js
+++ script.js
@@ -1,22 +1,26 @@
 import * as THREE from 'three';
 
+// ------- scene + camera -------
 const scene = new THREE.Scene();
 let width = window.innerWidth;
 let height = window.innerHeight;
 const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 
+// ------- renderer -------
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(width, height);
 document.body.appendChild( renderer.domElement );
 camera.position.set(0, 0, 80);
 
+// ------- Light -------
+
+// ------- Object -------
 const geo = new THREE.SphereGeometry( 15, 32, 16 );
-const mat = new THREE.MeshBasicMaterial( { color: 0xffff00 } );
+const mat = new THREE.MeshBasicMaterial( { color: 0x00ffff } );
 const mainSphere = new THREE.Mesh( geo, mat );
 scene.add( mainSphere );
 
-console.log(mainSphere + " sphere added")
-
+// ------- Responsive design -------
 window.addEventListener('resize', () => {
     width = window.innerWidth;
     height = window.innerHeight;
@@ -26,6 +30,8 @@ window.addEventListener('resize', () => {
     renderer.setSize(width, height);
 });
 
+
+// ------- animate -------
 function animate() {
 
   renderer.render( scene, camera );
