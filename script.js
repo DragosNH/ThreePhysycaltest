923b7f63e28f3c5622bc5137b9842961f9c16a55
Dragoş <edragos33@gmail.com>
2025-06-02 17:40:38 +0200

    added css + made page responsive

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
 
 const scene = new THREE.Scene();
-const width = window.innerWidth;
-const height = window.innerHeight;
+let width = window.innerWidth;
+let height = window.innerHeight;
 const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer();
@@ -15,7 +15,14 @@ const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
 const cube = new THREE.Mesh( geometry, material );
 scene.add( cube );
 
+window.addEventListener('resize', () => {
+    width = window.innerWidth;
+    height = window.innerHeight;
+    camera.aspect = width / height;
+    camera.updateProjectionMatrix();
 
+    renderer.setSize(width, height);
+});
 
 function animate() {
 
